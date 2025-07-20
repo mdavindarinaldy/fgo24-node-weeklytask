5bde0f8644dcf4bedd00be048ff48a9182ffdd9c
Naldy <mdavindarinaldy@gmail.com>
2025-07-20 15:07:36 +0700

    chore: update status code for clearer code readability

--- src/middlewares/validation.middleware.js
+++ src/middlewares/validation.middleware.js
@@ -1,10 +1,11 @@
 const { validationResult } = require("express-validator");
+const {constants: http} = require("http2");
 
 exports.handleValidation = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const messages = errors.array().map(err => err.msg);
-    return res.status(400).json({
+    return res.status(http.HTTP_STATUS_BAD_REQUEST).json({
       success: false,
       message: "Validation failed",
       errors: messages
