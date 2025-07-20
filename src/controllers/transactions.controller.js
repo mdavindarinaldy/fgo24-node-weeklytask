a4f22a693b336dbb3e399d7b4cfe64dd9aaf6c42
Naldy <mdavindarinaldy@gmail.com>
2025-07-21 01:13:02 +0700

    feat: create endpoint to add new transaction

--- src/controllers/transactions.controller.js
+++ src/controllers/transactions.controller.js
@@ -1,4 +1,4 @@
-const { Payment_Methods, Showtimes, Transaction_Detail } = require("../models");
+const { Payment_Methods, Showtimes, Transaction_Detail, Movie, Transactions, sequelize } = require("../models");
 const { constants: http } = require("http2");
 const { Sequelize } = require("sequelize");
 
@@ -89,6 +89,117 @@ exports.getReservedSeats = async (req, res) => {
       success: false,
       message: "Failed to get data reserved seats",
       errors: err.message,
+    });
+  }
+};
+
+exports.addTransaction = async (req, res) => {
+  const t = await sequelize.transaction();
+  const userId = req.userId;
+
+  try {
+    const {
+      movieId,
+      paymentMethodId,
+      location,
+      cinema,
+      date,
+      showtime,
+      seats
+    } = req.body;
+
+    if (!movieId || !paymentMethodId || !location || !cinema || !date || !showtime || !seats || !seats.length) {
+      return res.status(http.HTTP_STATUS_BAD_REQUEST).json({
+        success: false,
+        message: "Failed to order ticket",
+        errors: "transactions data should not be empty"
+      });
+    }
+
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(date)) {
+      return res.status(http.HTTP_STATUS_BAD_REQUEST).json({
+        success: false,
+        message: "Failed to order ticket",
+        errors: "invalid date format, use YYYY-MM-DD"
+      });
+    }
+
+    if (!/^\d{2}:\d{2}(:\d{2})?$/.test(showtime)) {
+      return res.status(http.HTTP_STATUS_BAD_REQUEST).json({
+        success: false,
+        message: "Failed to order ticket",
+        errors: "invalid showtime format, use HH:MM:SS"
+      });
+    }
+
+    let showtimeData = await Showtimes.findOne({
+      where: {
+        id_movie: movieId,
+        location,
+        cinema,
+        date,
+        showtime
+      },
+      transaction: t
+    });
+
+    if (!showtimeData) {
+      showtimeData = await Showtimes.create({
+        id_movie: movieId,
+        location,
+        cinema,
+        date,
+        showtime
+      }, { transaction: t });
+    }
+
+    const movie = await Movie.findByPk(movieId);
+    if (!movie) {
+      return res.status(http.HTTP_STATUS_NOT_FOUND).json({
+        success: false,
+        message: "Failed to order ticket",
+        errors: "Movie not found"
+      });
+    }
+
+    const price = movie.price;
+    const totalAmount = price * seats.length;
+
+    const transaction = await Transactions.create({
+      id_user: userId,
+      id_payment_method: paymentMethodId,
+      total_price: totalAmount
+    }, { transaction: t });
+
+    const trxDetailPayload = seats.map(seat => ({
+      id_transaction: transaction.id,
+      id_showtime: showtimeData.id,
+      seat
+    }));
+    await Transaction_Detail.bulkCreate(trxDetailPayload, { transaction: t });
+
+    await t.commit();
+    return res.status(201).json({
+      success: true,
+      message: "Success to order ticket",
+      result: {
+        showtimeId: showtimeData.id,
+        transactionId: transaction.id
+      }
+    });
+  } catch(err) {
+    await t.rollback();
+    if (err.name === "SequelizeUniqueConstraintError") {
+      return res.status(http.HTTP_STATUS_BAD_REQUEST).json({
+        success: false,
+        message: "Failed to order ticket",
+        errors: "Seat(s) are already taken"
+      });
+    }
+    console.error(err);
+    return res.status(http.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Internal server error"
     });
   }
 };
--- src/routers/transaction.router.js
+++ src/routers/transaction.router.js
@@ -3,5 +3,6 @@ const transactionController = require("../controllers/transactions.controller");
 
 transactionRouter.get("/payment-methods", transactionController.getPaymentMethods);
 transactionRouter.get("/seats", transactionController.getReservedSeats);
+transactionRouter.post("", transactionController.addTransaction);
 
 module.exports = transactionRouter;
