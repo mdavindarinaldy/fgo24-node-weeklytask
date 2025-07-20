11f01f28923d3163d07931c6fcf46d232bfa8f45
Naldy <mdavindarinaldy@gmail.com>
2025-07-21 01:44:28 +0700

    feat: create endpoint to get sales data per movie

--- src/controllers/adminTransaction.controller.js
+++ src/controllers/adminTransaction.controller.js
@@ -1,5 +1,6 @@
-const { Payment_Methods } = require("../models");
+const { Payment_Methods, sequelize } = require("../models");
 const { constants: http } = require("http2");
+const { QueryTypes } = require("sequelize");
 
 exports.addPaymentMethod = async (req, res) => {
   try {
@@ -29,6 +30,42 @@ exports.addPaymentMethod = async (req, res) => {
     return res.status(http.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
       success: false,
       message: "Status internal server error",
+    });
+  }
+};
+
+exports.getSalesData = async (req, res) => {
+  const role = req.role;
+  if (role !== "admin") {
+    return res.status(http.HTTP_STATUS_FORBIDDEN).json({
+      success: false,
+      message: "Forbidden",
+    });
+  }
+
+  try {
+    const result = await sequelize.query(`
+      SELECT 
+        m.id AS id_movie,
+        m.title,
+        COUNT(td.seat) AS tickets_sold,
+        m.price AS price_per_ticket,
+        COUNT(td.seat) * m.price AS total_amount
+      FROM transactions_detail td
+      JOIN showtimes s ON s.id = td.id_showtime
+      JOIN movies m ON m.id = s.id_movie
+      GROUP BY m.id, m.title, m.price
+    `, { type: QueryTypes.SELECT });
+    return res.status(http.HTTP_STATUS_OK).json({
+      success: true,
+      message: "Success to get sales data",
+      result,
+    });
+  } catch(err) {
+    console.error(err);
+    return res.status(http.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Internal server error",
     });
   }
 };
--- src/routers/adminTransaction.router.js
+++ src/routers/adminTransaction.router.js
@@ -2,6 +2,6 @@ const adminTransactionRouter = require("express").Router();
 const adminTransactionController = require("../controllers/adminTransaction.controller");
 
 adminTransactionRouter.post("/payment-methods", adminTransactionController.addPaymentMethod);
-// adminTransactionRouter.get("/sales");
+adminTransactionRouter.get("/sales", adminTransactionController.getSalesData);
 
 module.exports = adminTransactionRouter;
