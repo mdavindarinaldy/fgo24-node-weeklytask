0a7c8cfd8f2dfeeda6d2112551b182086f3a064e
Naldy <mdavindarinaldy@gmail.com>
2025-07-20 14:30:14 +0700

    feat: modified register endpoint

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const {constants: http} = require("http2");
-const {User} = require("../models");
+const {sequelize, User, Profile} = require("../models");
 const { validationResult } = require("express-validator");
+const argon2 = require("argon2");
 
 exports.login = async function(req, res){
   const {email, password} = req.body;
@@ -30,21 +31,69 @@ exports.login = async function(req, res){
 };
 
 exports.register = async function(req, res) {
-  const {email, password} = req.body;
-  const filename = req.file ? req.file.filename : null;
-  const found = await User.findOne({where: {email:email}});
-  if (!found) {
-    const user = await User.create({email: email, password: password, picture: filename});
-    const responseUser = {id:user.id, email:user.email, picture:user.picture};
-    return res.status(http.HTTP_STATUS_CREATED).json({
-      success: true,
-      message: "User berhasil melakukan registrasi",
-      result: responseUser
-    });
-  } else {
-    return res.status(http.HTTP_STATUS_CONFLICT).json({
+  const { name, email, phoneNumber, password, confPass } = req.body;
+  if (!name || !email || !phoneNumber || !password || !confPass) {
+    return res.status(400).json({
       success: false,
-      message: "Email sudah digunakan"
+      message: "Failed to register user",
+      errors: "user data should not be empty",
+    });
+  }
+
+  if (password !== confPass) {
+    return res.status(400).json({
+      success: false,
+      message: "Failed to register user",
+      errors: "password and confirm password doesn't match",
+    });
+  }
+
+  try {
+    const hashedPassword = await argon2.hash(password);
+    await sequelize.transaction(async (t) => {
+      const user = await User.create({
+        email,
+        password: hashedPassword,
+        role: "user",
+        created_at: new Date(),
+      }, { transaction: t });
+
+      await Profile.create({
+        id_user: user.id,
+        name,
+        phone_number: phoneNumber,
+        created_at: new Date(),
+      }, { transaction: t });
+
+      return user;
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Create user success!",
+      result: {
+        name,
+        email,
+        phoneNumber,
+      }
+    });
+  } catch (err) {
+    if (err.name === "SequelizeUniqueConstraintError") {
+      const errorMsg = err.errors[0].message.includes("email")
+        ? "email already used by another user"
+        : "phone number already used by another user";
+
+      return res.status(400).json({
+        success: false,
+        message: "Failed to register user",
+        errors: errorMsg,
+      });
+    }
+
+    console.error(err);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
     });
   }
 };
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -1,23 +1,24 @@
 const authRouter = require("express").Router();
 const authContoller = require("../controllers/auth.controller");
 const authDto = require("../dto/auth.dto");
-const path = require("node:path");
-const multer = require("multer");
-const {v4: uuid} = require("uuid");
+// const path = require("node:path");
+// const multer = require("multer");
+// const {v4: uuid} = require("uuid");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb)=> {
-    cb(null, path.join("uploads","profile-picture"));
-  },
-  filename: (req, file, cb)=>{
-    const ext = path.extname(file.originalname);
-    const savedFile = `${uuid()}${ext}`;
-    cb(null, savedFile);
-  }
-});
-const profilePicture = multer({storage});
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb)=> {
+//     cb(null, path.join("uploads","profile-picture"));
+//   },
+//   filename: (req, file, cb)=>{
+//     const ext = path.extname(file.originalname);
+//     const savedFile = `${uuid()}${ext}`;
+//     cb(null, savedFile);
+//   }
+// });
+// const profilePicture = multer({storage});
 
 authRouter.post("/login", authDto.login, authContoller.login);
-authRouter.post("/register", profilePicture.single("picture"), authContoller.register);
+// authRouter.post("/register", profilePicture.single("picture"), authContoller.register);
+authRouter.post("/register", authContoller.register);
 
 module.exports = authRouter;
