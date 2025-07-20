fe1e936d1679a7260d134ce13d37e533118292ea
Naldy <mdavindarinaldy@gmail.com>
2025-07-21 00:48:54 +0700

    feat: create endpoint to get reserved seats

--- src/controllers/transactions.controller.js
+++ src/controllers/transactions.controller.js
@@ -1,5 +1,6 @@
-const { Payment_Methods } = require("../models");
+const { Payment_Methods, Showtimes, Transaction_Detail } = require("../models");
 const { constants: http } = require("http2");
+const { Sequelize } = require("sequelize");
 
 exports.getPaymentMethods = async (req, res) => {
   try {
@@ -17,6 +18,77 @@ exports.getPaymentMethods = async (req, res) => {
     res.status(http.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
       success: false,
       message: "Failed to get data",
+    });
+  }
+};
+
+exports.getReservedSeats = async (req, res) => {
+  try {
+    const { id_movie, cinema, location, date, showtime } = req.query;
+    if (!id_movie || !cinema || !location || !date || !showtime) {
+      return res.status(http.HTTP_STATUS_BAD_REQUEST).json({
+        success: false,
+        message: "Failed to get data reserved seats",
+        errors: "All fields must be provided",
+      });
+    }
+
+    const showtimeData = await Showtimes.findOne({
+      where: {
+        id_movie,
+        cinema,
+        location,
+        date,
+        showtime,
+      },
+    });
+
+    if (!showtimeData) {
+      return res.status(http.HTTP_STATUS_OK).json({
+        success: true,
+        message: "Success to get data reserved seats",
+        result: {
+          id_showtime: 0,
+          seats: "",
+        },
+      });
+    }
+
+    const showtimeId = showtimeData.id;
+    const result = await Transaction_Detail.findAll({
+      attributes: [
+        "id_showtime",
+        [Sequelize.fn("STRING_AGG", Sequelize.col("seat"), ", "), "seats"],
+      ],
+      where: { id_showtime: showtimeId },
+      group: ["id_showtime"],
+      raw: true,
+    });
+    if (result.length === 0) {
+      return res.status(http.HTTP_STATUS_OK).json({
+        success: true,
+        message: "Success to get data reserved seats",
+        result: {
+          id_showtime: showtimeId,
+          seats: "",
+        },
+      });
+    }
+
+    return res.status(http.HTTP_STATUS_OK).json({
+      success: true,
+      message: "Success to get data reserved seats",
+      result: {
+        id_showtime: result[0].id_showtime,
+        seats: result[0].seats,
+      },
+    });
+  } catch(err) {
+    console.error(err);
+    return res.status(http.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Failed to get data reserved seats",
+      errors: err.message,
     });
   }
 };
--- src/routers/transaction.router.js
+++ src/routers/transaction.router.js
@@ -2,5 +2,6 @@ const transactionRouter = require("express").Router();
 const transactionController = require("../controllers/transactions.controller");
 
 transactionRouter.get("/payment-methods", transactionController.getPaymentMethods);
+transactionRouter.get("/seats", transactionController.getReservedSeats);
 
 module.exports = transactionRouter;
